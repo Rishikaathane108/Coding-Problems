add9138eeb4b372b956f58e59b5f41f8094f0bd3
Rishikaathane108 <91424358+Rishikaathane108@users.noreply.github.com>
2023-08-20 19:41:02 +0530

    Updated Solution - GfG to GitHub

--- Medium/Number of occurrence/number-of-occurrence.cpp
+++ Medium/Number of occurrence/number-of-occurrence.cpp
@@ -12,72 +12,13 @@ public:
 		of occurrences of x, otherwise returns 0. */
 	int count(int arr[], int n, int x) {
 	    // code here
-	    int low=0;
-	    int high=n-1;
-	    int first=-1,last;
-	    
-	    first=firstOcc(arr,low,high,x);
-	    
-	    if(first==-1) return 0;
-	    else
-	    {
-	        last=lastOcc(arr,low,high,x);
-	    }
-	    
-	    int repeat=(last-first)+1;
-	    
-	    return repeat;
-	    
-	}
-	
-	int firstOcc(int arr[], int low, int high, int x)
-	{
-	    int first=-1;
-	    while(low<=high)
-	    {
-	        int mid=(low+high)/2;
-	        
-	        if(arr[mid]==x)
-	        {
-	            first=mid;
-	            high=mid-1;
-	        }
-	        else if(arr[mid]>x)
-	        {
-	            high=mid-1;
-	        }
-	        else
-	        {
-	            low=mid+1;
-	        }
-	    }
-	    
-	    return first;
-	}
-	
-	int lastOcc(int arr[], int low, int high, int x)
-	{
-	    int last;
-	    while(low<=high)
-	    {
-	        int mid=(low+high)/2;
-	        
-	        if(arr[mid]==x)
-	        {
-	            last=mid;
-	            low=mid+1;
-	        }
-	        else if(arr[mid]>x)
-	        {
-	            high=mid-1;
-	        }
-	        else
-	        {
-	            low=mid+1;
-	        }
-	    }
-	    
-	    return last;
+	    int occur=0;
+     for(int i=0;i<n;i++)
+     {
+        if(arr[i]==x)
+        occur++;
+     }
+  return occur;
 	}
 };
 
